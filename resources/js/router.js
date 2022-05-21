6f983f00113604ad654da2fde0692d485f197f65
nvlight <lightstyle11@mail.ru>
2022-05-21 10:15:26 +0300

    install vuex and some small stuff

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,12 +1,36 @@
 import { createRouter, createWebHistory} from 'vue-router';
 import Container from "./views/layout/Container";
+import Register from "./views/Auth/Register";
+import Login from "./views/Auth/Login";
+import Dashboard from "./views/pages/Dashboard";
+import UserProfile from "./views/pages/UserProfile";
 
 const routes = [
     {
         path: '/',
         name: Container,
         component: Container,
-    }
+    },
+    {
+        path: "/login",
+        name: Login,
+        component: Login,
+    },
+    {
+        path: "/register",
+        name: Register,
+        component: Register,
+    },
+    {
+        path: "/dashboard",
+        name: Dashboard,
+        component: Dashboard,
+    },
+    {
+        path: "/user-profile",
+        name: UserProfile,
+        component: UserProfile,
+    },
 ]
 
 const router = createRouter({
