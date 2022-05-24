4e6f2f4e882290b4ef82f326ab3d9c7394a467a6
nvlight <lightstyle11@mail.ru>
2022-05-24 12:22:48 +0300

    add forgot password

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\AuthController;
+use \App\Http\Controllers\Api\ForgotPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('register', [AuthController::class,'register']);
 Route::post('login',    [AuthController::class,'login']);
+Route::post('forgot-password',    [ForgotPasswordController::class,'sendResetLinkEmail']);
